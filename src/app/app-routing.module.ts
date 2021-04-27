2a065f2c497af3532ac026ac9f00b4d9b0384315
Andressa Novaes <isa.menezes@ilia.digital>
2021-04-27 05:40:40 -0300

    feat: glass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GlassComponent} from './components/glass/glass.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GlassComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { GlassComponent } from './components/glass/glass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlassComponent
   ],
   imports: [
     BrowserModule,
