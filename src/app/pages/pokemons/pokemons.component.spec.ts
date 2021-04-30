3e860dacf642e2676c8193ec2f449e85ba00d9e5
Andressa Novaes <isa.menezes@ilia.digital>
2021-04-30 10:14:23 -0300

    test: pokemons

--- src/app/pages/pokemons/pokemons.component.spec.ts
+++ src/app/pages/pokemons/pokemons.component.spec.ts
@@ -1,21 +1,56 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { PokemonsComponent } from './pokemons.component';
-import { Pokemon } from '../../models/pokemon'
+import { TranslateTestingModule } from 'ngx-translate-testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { TranslateService } from '@ngx-translate/core';
 
-describe('PokemonsComponent', () => {
+fdescribe('PokemonsComponent', () => {
   let component: PokemonsComponent;
   let fixture: ComponentFixture<PokemonsComponent>;
+  let mockPokemonService: PokemonService;
 
+  const pokemonMock = {
+    id: "TEST_ID",
+    name: "TEST_NAME",
+    types: ["TEST_TYPES"],
+    images: {
+      large: "",
+      small: "src/assets/img/card-test.png"
+    },
+  };
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PokemonsComponent ]
+      declarations: [ PokemonsComponent ],
+      imports: [
+        TranslateTestingModule.withTranslations({ pt: require('src/assets/i18n/pt.json'), en: require('src/assets/i18n/en.json') }),
+        HttpClientTestingModule ,
+      ],
+      providers: [
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            queryParams: { id: pokemonMock.id },
+          },
+        },
+      }
+    ],
     })
     .compileComponents();
+    mockPokemonService = TestBed.inject(PokemonService);
   }));
+
+  afterEach(() => {
+    fixture.destroy();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PokemonsComponent);
+    fixture.componentInstance.pokemons = [pokemonMock];
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -24,11 +59,11 @@ describe('PokemonsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-    it(`should have as title 'pokemont'`, () => {
-    const fixture = TestBed.createComponent(PokemonsComponent);
-    const pokemonsContain = fixture.componentInstance;
-
-   // expect(pokemonsContain.pokemons).toEqual(jasmine.arrayContaining([]));
-  });
+  it('should render list pokemon', inject([TranslateService], (translateService: TranslateService) => {
+    const compiled = fixture.debugElement.nativeElement;
+    const list = compiled.querySelector('#list-pokemon');
+    
+    expect(list.textContent).toContain(pokemonMock.name);
+  }));
 
 });
