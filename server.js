75e4082de99619e7a9a5142f0741f7d69c07355e
Rhuan Pablo <rhuanpablo13@hotmail.com>
2021-04-30 22:16:11 -0300

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+//const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-});
-
+app.use(express.static('./dist/pokemont'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/pokemont/'}),
+);
 app.listen(process.env.PORT || 8080);
+
+// app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+// app.get('/*', (req, res) => {
+// res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+// });
+
+// app.listen(process.env.PORT || 8080);
